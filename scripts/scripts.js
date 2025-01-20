10586f768f205adb1031eb572fd96b24f61a4241
adsharma2910 <adsharma2910github@gmail.com>
2025-01-20 14:48:29 +1100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,17 +39,7 @@ async function loadFonts() {
     // do nothing
   }
 }
-function autolinkModals(element) {
-  element.addEventListener('click', async (e) => {
-    const origin = e.target.closest('a');
 
-    if (origin && origin.href && origin.href.includes('/modals/')) {
-      e.preventDefault();
-      const { openModal } = await import(`${window.hlx.codeBasePath}/blocks/modal/modal.js`);
-      openModal(origin.href);
-    }
-  });
-}
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
